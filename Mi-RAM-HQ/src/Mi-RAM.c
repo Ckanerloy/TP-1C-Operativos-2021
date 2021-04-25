d76cb29401491c0870ffd75e01fedfed70333df3
Luciano Capomolla <lucho.capomolla@hotmail.com>
2021-04-25 18:13:35 -0300

    Agregando los demas módulos

---
+++ Mi-RAM-HQ/src/Mi-RAM.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Mi-RAM.c
+ Author      : Luciano
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
